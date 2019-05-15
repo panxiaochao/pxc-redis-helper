40725b5b80c8e951da7d86b1e12afc07e2bade4c
panxiaochao602@qq.com <panxiaochao602@qq.com>
2019-05-15 11:39:01 +0800

    first commit

---
+++ src/main/java/com/github/panxiaochao/pxc_redis_helper/App.java
@@ -0,0 +1,13 @@
+package com.github.panxiaochao.pxc_redis_helper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
